5c8adc23b32e02747f0191d49893ed666103a547
JN <joyng808@gmail.com>
2024-06-14 13:14:18 +0800

    [JN]-Updated API

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -46,8 +46,13 @@ namespace API.Extensions
             };
     });
 
-
-
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
 
             return services;
         }
