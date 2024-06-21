3ebd2f2b93eec791cdaa3b6b8b00be42f5695aad
JN <joyng808@gmail.com>
2024-06-21 19:29:28 +0800

    [JN]-Added Initial View HTML using Boostrap

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Product } from './models/products';
-import { Pagination } from './models/pagination';
+
 
 @Component({
   selector: 'app-root',
@@ -10,20 +8,12 @@ import { Pagination } from './models/pagination';
 })
 export class AppComponent implements OnInit{
   title = 'BeanTea';
-  products:Product[] = [];
+ 
 
-  constructor(private http: HttpClient){}
+  constructor(){}
 
 
   ngOnInit(): void {
-    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pageSize=50').subscribe({
-      next: response =>  this.products= response.data,//console.log(response), // what to do next 
-      error : error => console.log(error), // what to do if there is an error 
-      complete : () => {
-        console.log('request completed');
-        console.log('extra statement')
-      }
-    })
   }
 }
 
