2107cb2bdd08da36caeef1d1164ed853a484e08c
JN <joyng808@gmail.com>
2024-06-21 12:27:28 +0800

    [JN]-Update Basic Angular

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Product } from './models/products';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit{
   title = 'BeanTea';
-  products: any[] =[];
+  products:Product[] = [];
 
   constructor(private http: HttpClient){}
 
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe({
-      next: (response: any) =>  this.products= response.data,//console.log(response), // what to do next 
+    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pageSize=50').subscribe({
+      next: response =>  this.products= response.data,//console.log(response), // what to do next 
       error : error => console.log(error), // what to do if there is an error 
       complete : () => {
         console.log('request completed');
---
+++ client/src/app/models/products.ts
@@ -0,0 +1,26 @@
+
+export interface Product {
+  id: number
+  name: string
+  description: string
+  price: number
+  quantity_In_Stock: number
+  origin: string
+  type: string
+  flavor: string
+  caffeineContent: string
+  imageUrl: string
+  manufacturingDate: string
+  expirationDate: string
+  steepingTime?: string
+  waterTemperature?: string
+  color?: string
+  caffeineLevel?: string
+  healthBenefits: string
+  roastLevel?: string
+  grindType?: string
+  brewingMethod?: string
+  productType: string
+  productBrand: string
+}
+
