687b270661cbf4252d2e35ce53c1f98371d037c2
JN <joyng808@gmail.com>
2024-07-15 09:26:27 +0800

    [JN]-Added Loading property and Home page carousel

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -13,7 +13,10 @@ import { BreadcrumbService } from 'xng-breadcrumb';
 export class ProductDetailsComponent implements OnInit{
   product?: Product;
 
-  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute, private bcService : BreadcrumbService){}
+  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute,
+     private bcService : BreadcrumbService){
+      this.bcService.set('@productDetails',' ')
+     }
 
   ngOnInit(): void {
     this.loadProduct();
