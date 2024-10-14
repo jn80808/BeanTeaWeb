9589dbf8001dd644305e4109a7899adf0fa36db8
JN <joyng808@gmail.com>
2024-10-14 21:14:21 +0800

    [JN]-Added new Functionality and Updated FE View in View

--- client/src/app/account/register/register.component.ts
+++ client/src/app/account/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { AccountService } from '../account.service';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +10,21 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  constructor(private fb: FormBuilder, private accountService: AccountService, private router: Router) {}
+
+  // Updated regex for password requirements to allow any length
+  complexPassword = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+}{\":;'?/>,.<])(?!.*\\s).*$";
+
+  registerForm = this.fb.group({
+    displayName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.pattern(this.complexPassword)]],
+  });
+
+  
+  onSubmit() {
+    this.accountService.register(this.registerForm.value).subscribe({
+      next: () => this.router.navigateByUrl('/shop')
+    });
+  }
 }
