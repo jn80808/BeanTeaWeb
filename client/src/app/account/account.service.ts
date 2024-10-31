5660e3d9fe5e13fdea6bb9d6bd1f95f2537da1f0
JN <joyng808@gmail.com>
2024-10-31 16:24:12 +0800

    [JN]- Added Using Replay

--- client/src/app/account/account.service.ts
+++ client/src/app/account/account.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, map } from 'rxjs';
+import { BehaviorSubject, map, of, ReplaySubject } from 'rxjs';
 import { environment } from 'src/environment/environment';
 import { User } from '../shared/models/user';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
@@ -11,22 +11,34 @@ import { Router } from '@angular/router';
 export class AccountService {
 
   baseUrl = environment.apiUrl;
-  private currentUserSource = new BehaviorSubject< User | null> (null);
+  private currentUserSource = new ReplaySubject< User | null> (1);
   currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http: HttpClient, private router: Router) { }
 
   // PersistentLogIn
-  loadCurrentUser(token : string){
+  loadCurrentUser(token : string | null){
+    if (token == null){
+      this.currentUserSource.next(null);
+      return of(null);
+    }
+
+
     let headers = new HttpHeaders();
     headers = headers.set('Authorization',`Bearer ${token}`);
   
-    return this.http.get<User>(this.baseUrl + 'account',{headers}).pipe(
-      map (user =>{
-        localStorage.setItem('token', user.token);
-        this.currentUserSource.next(user);  // This will update the currentUserSource
+    //get user
+    return this.http.get<User>(this.baseUrl + 'account', { headers }).pipe(
+      map(user => {
+        if (user) {
+          localStorage.setItem('token', user.token);
+          this.currentUserSource.next(user);  // This updates the currentUserSource
+          return user
+        } else {
+          return null;
+        }
       })
-    )
+    );
   }
 
   login(value: any){
--- client/src/app/account/login/login.component.ts
+++ client/src/app/account/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AccountService } from '../account.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -15,13 +15,17 @@ export class LoginComponent {
     password: new FormControl('', [Validators.required]) 
   });
 
+  returnUrl: string;
 
-  constructor(private accountService: AccountService, private router: Router){}
+  constructor(private accountService: AccountService, private router: Router,
+    private activatedRoute:ActivatedRoute){
+      this.returnUrl = this.activatedRoute.snapshot.queryParams['returnUrl'] || '/shop'
+    }
 
 
   onSubmit() {
     this.accountService.login(this.loginForm.value).subscribe({
-      next:() => this.router.navigateByUrl('/shop')
+      next:() => this.router.navigateByUrl(this.returnUrl)
     })
 
     }
